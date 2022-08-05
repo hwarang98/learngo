4a21c0707c452c5ccdaea84541a1d5752cdb91c6
LeeHwaRang <snow2271@gmail.com>
2022-08-05 22:33:04 +0900

    go study

--- Desktop/coding/Algorithm/stringSort.js
+++
@@ -1,8 +0,0 @@
-function solution(s) {
-    console.log('s 시작: ',
-        s.split('').sort().reverse().join('')
-    )
-    
-}
-
-solution('Zbcdefg');
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"learnGo/something"
+)
+
+func main()  {
+	fmt.Println("hello  go!")
+	something.SayHello()
+}
